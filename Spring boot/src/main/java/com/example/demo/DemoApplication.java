f5dfd535225747f9509cffed897c560922344e2b
yoonsung <skawk5678@gmail.com>
2018-09-26 18:33:08 +0900

    test for spring-boot

--- Spring boot/src/main/java/com/example/demo/DemoApplication.java
+++ Spring boot/src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import com.example.demo.client.dao.TestDao;
 import com.example.demo.client.model.DeptVO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +14,10 @@ public class DemoApplication {
     private TestDao testDao;
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+    @RequestMapping(value="/",method= RequestMethod.GET)
+    public String boot(){
+        return "hi boot!";
     }
 
 }
